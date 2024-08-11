a55a16ff73991852158adf75a8f1863c963d1089
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-08-11 12:36:10 -0300

    aula 3

--- investidores/views.py
+++ investidores/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.http import Http404
+from django.shortcuts import redirect, render
 
-from empresarios.models import Empresas
+from empresarios.models import Documento, Empresas
+
+from .models import PropostaInvestimento
 
 
 def sugestao(request):
+    if not request.user.is_authenticated:
+        return redirect("usuarios/logar")
     areas = Empresas.area_choices
     if request.method == "GET":
         return render(request, "sugestao.html", {"areas": areas})
@@ -34,3 +41,103 @@ def sugestao(request):
             "sugestao.html",
             {"empresas": empresas_selecionadas, "areas": areas},
         )
+
+
+def ver_empresa(request, id):
+    empresa = Empresas.objects.get(id=id)
+    documentos = Documento.objects.filter(empresa=empresa)
+    proposta_investimentos = PropostaInvestimento.objects.filter(
+        empresa=empresa
+    ).filter(status="PA")
+    percentual_vendido = 0
+    for pi in proposta_investimentos:
+        percentual_vendido = percentual_vendido + pi.percentual
+
+    limiar = (80 * empresa.percentual_equity) / 100
+    concretizado = False
+    if percentual_vendido >= limiar:
+        concretizado = True
+
+    percentual_disponivel = empresa.percentual_equity - percentual_vendido
+
+    return render(
+        request,
+        "ver_empresa.html",
+        {
+            "empresa": empresa,
+            "documentos": documentos,
+            "percentual_vendido": int(percentual_vendido),
+            "concretizado": concretizado,
+            "percentual_disponivel": percentual_disponivel,
+        },
+    )
+
+
+def realizar_proposta(request, id):
+    valor = request.POST.get("valor")
+    percentual = request.POST.get("percentual")
+    empresa = Empresas.objects.get(id=id)
+
+    propostas_aceitas = PropostaInvestimento.objects.filter(
+        empresa=empresa
+    ).filter(status="PA")
+    total = 0
+
+    for pa in propostas_aceitas:
+        total = total + pa.percentual
+
+    if total + float(percentual) > empresa.percentual_equity:
+        messages.add_message(
+            request,
+            constants.WARNING,
+            "O percentual solicitado ultrapassa o percentual máximo.",
+        )
+        return redirect(f"/investidores/ver_empresa/{id}")
+
+    valuation = (100 * int(valor)) / int(percentual)
+
+    if valuation < (float(empresa.valuation) / 2):
+        divisao = float(empresa.valuation) / 2
+        messages.add_message(
+            request,
+            constants.WARNING,
+            f"Seu valuation proposto foi R${valuation} e deve ser no mínimo R${divisao}",
+        )
+        return redirect(f"/investidores/ver_empresa/{id}")
+
+    pi = PropostaInvestimento(
+        valor=valor,
+        percentual=percentual,
+        empresa=empresa,
+        investidor=request.user,
+    )
+
+    pi.save()
+
+    # messages.add_message(request, constants.SUCCESS, f'Proposta enviada com sucesso')
+    return redirect(f"/investidores/assinar_contrato/{pi.id}")
+
+
+def assinar_contrato(request, id):
+    pi = PropostaInvestimento.objects.get(id=id)
+    if pi.status != "AS":
+        raise Http404()
+
+    if request.method == "GET":
+        return render(request, "assinar_contrato.html", {"id": id, "pi": pi})
+    elif request.method == "POST":
+        selfie = request.FILES.get("selfie")
+        rg = request.FILES.get("rg")
+        print(request.FILES)
+
+        pi.selfie = selfie
+        pi.rg = rg
+        pi.status = "PE"
+        pi.save()
+
+        messages.add_message(
+            request,
+            constants.SUCCESS,
+            f"Contrato assinado com sucesso, sua proposta foi enviada a empresa.",
+        )
+        return redirect(f"/investidores/ver_empresa/{pi.empresa.id}")
